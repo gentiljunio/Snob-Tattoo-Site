e948bb2d68dd3b37070de3770c693c6392597a9a
Gentil Junio <88815184+gentiljunio@users.noreply.github.com>
2026-03-04 00:16:54 -0300

    Update video pause time to 22 seconds
    Change video playback behavior on mouse leave.

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// Seleciona todos os cards da galeria
 const videoCards = document.querySelectorAll('.card');
 
 videoCards.forEach(card => {
   const video = card.querySelector('.insta-video');
 
-  // Ao passar o mouse: Play
   card.addEventListener('mouseenter', () => {
     video.play().catch(error => {
       console.log("O autoplay foi bloqueado pelo navegador:", error);
     });
   });
-
-  // Ao tirar o mouse: Pause e volta pro início
+  
   card.addEventListener('mouseleave', () => {
     video.pause();
-    video.currentTime = 0; 
+    video.currentTime = 22;
   });
 });
